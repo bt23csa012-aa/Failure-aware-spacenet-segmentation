d7d9e6d6d025287625bcd4b209493fa101ba0f16
bt23csa012-aa <bt23csa012@iiitn.ac.in>
2025-12-19 08:25:29 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,16 @@
+import torch
+import torch.nn as nn
 
+class SimpleUNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.enc = nn.Sequential(
+            nn.Conv2d(3, 16, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.dec = nn.Conv2d(32, 1, 1)
+
+    def forward(self, x):
+        return torch.sigmoid(self.dec(self.enc(x)))
